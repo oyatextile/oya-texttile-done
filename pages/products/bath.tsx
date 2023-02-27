db4398a792557c6191ce0c7bce35e8cec31a8f9b
OYATETXILE <118007189+oyatextile@users.noreply.github.com>
2023-02-27 20:31:52 +0100

    replacing ' with &apos;

--- pages/products/bath.tsx
+++ pages/products/bath.tsx
@@ -50,8 +50,8 @@ const HeadCat = () => {
         margin={"auto"}
         maxW="2xl"
       >
-        OYA’s products for bathing are manufactured with standards, in addition
-        to a competitive price to match your country’s market requirements and
+        OYA&apos;s products for bathing are manufactured with standards, in addition
+        to a competitive price to match your country&apos;s market requirements and
         needs.
         Subscribe to our newsletter 
         <a href="https://www.linkedin.com/newsletters/home-textile-manufacturing-7013520608349024257/">on Linkedin </a> 
